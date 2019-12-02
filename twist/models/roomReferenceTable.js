40260844452bc8359a613bf9ace3dbe0a3b10811
Leaderfirestar <EricAWebb2000@yahoo.com>
2019-12-02 08:40:03 -0600

    Removed a comment

--- twist/models/roomReferenceTable.js
+++ twist/models/roomReferenceTable.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-//Could be wrong, but I think convention with Schemas is to capitalize
 var RoomReferenceSchema = new Schema(
   {
     roomNumber: {type: String, required: true},
