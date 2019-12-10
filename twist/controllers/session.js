3f5c44ad6a32a4819c034006cec0216b26845574
Alex Windhorst <windhorstalex@gmail.com>
2019-12-10 13:07:36 -0600

    added to session.js controller

---
+++ twist/controllers/session.js
@@ -0,0 +1,26 @@
+let perSessionInfo = reqire('../models/perSessionInfo');
+const {sanitizeBody} = require('express-validator');
+
+exports.new_session = [
+    sanitizeBody('sessionId').escape(),
+    sanitizeBody('time').escape(),
+    sanitizeBody('roomNumber').escape(),
+    sanitizeBody('topicId').escape(),
+    sanitizeBody('speakerId').escape(),
+    sanitizeBody('blockId').escape(),
+    sanitizeBody('participants').escape(),
+    (req, res, next) => {
+        var newSession = new session({
+        sessionId: req.body.sessionId,
+        time: req.body.time,
+        roomNumber: req.body.roomNumber,
+        topicId: req.body.topicId,
+        speakerId: req.body.speakerId,
+        blockId: req.body.blockId,
+        participants: req.body.participants
+        });
+        newSession.save(function(err){
+            if (err) {return next(err);}
+        })
+    }
+]
