980ff769ed4b25ae18a3709913eebb828751f4cc
Jon Martin <gametime785@gmail.com>
2019-12-12 10:01:52 -0600

    Updated Session Model.

--- twist/models/perSessionInfo.js
+++ twist/models/perSessionInfo.js
@@ -4,8 +4,6 @@ var Schema = mongoose.Schema;
 
 var PerSessionSchema = new Schema(
   {
-    sessionId: {type: Schema.Types.ObjectId, required: true},
-    time: {type: Date, required: true},
     roomNumber: {type: String, ref: 'roomReferenceTable', required: true},
     topicId: {type: Schema.Types.ObjectId, ref: 'topicReferenceTable', required: true},
     speakerId: {type: Schema.Types.ObjectId, ref: 'speaker', required: true},
