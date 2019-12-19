0950bc00eba4a9d4067a708105aff8e5366ded74
elliottwesley <aquariuswre@ksu.edu>
2019-12-18 22:09:51 -0600

    Added files and changes required for deployment to Heroku.

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 app.use(session({secret: 'secure', saveUninitialized: true, resave: true}));
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://leaderfirestar:6521kertratS@twist-u3qyx.mongodb.net/test?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://leaderfirestar:6521kertratS@twist-u3qyx.mongodb.net/test?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
